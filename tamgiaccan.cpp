a0985ed94f81e89e6d8f41455577cb7535c365fc
nguyenhuy07-09-2k1 <72030082+nguyenhuy07-09-2k1@users.noreply.github.com>
2020-11-26 10:51:30 +0700

    Tham tam giac can C++

---
+++ tamgiaccan.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+	int n,q=0;
+	printf("Nhap vao chieu cao: ");
+	scanf("%d",&n);
+	
+	while(n>0)
+	{
+	
+		for(int i=1;i<n;i++) printf("%c",' ');
+	
+		for(int k=0;k<=q;k++) printf("%c",'*');
+		
+		for(int k=0;k<q;k++) printf("%c",'*');
+		
+		n--;
+		q++;
+		
+		printf("\n");
+			
+	}
+	
+	getch();
+}
